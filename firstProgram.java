31c0b8232a29eb343033e1f65449490ed5fc0ea3
MohdAbdulRaheem45 <mohammedabdulr373@gmail.com>
2025-08-10 21:09:06 +0530

    Added program

---
+++ firstProgram.java
@@ -0,0 +1,13 @@
+package Localrepo;
+
+public class firstProgram {
+    public static void main(String[] args) {
+        int age=17;
+        if(age>=18){
+            System.out.println("eligible for vote");
+        }else{
+            System.out.println("not eligible for vote");
+        }
+    }
+    
+}
